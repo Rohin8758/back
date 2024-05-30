8565b3f0006c03f764e8d8c199bb4a2a6ba65239
Rohin8758 <rohinbambhroliya8758@gmail.com>
2024-05-30 08:50:54 +0530

    first commit

---
+++ model/product.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    image: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    productType: {
+      type: String,
+      enum: ["hot item", "new arrival", "on sale"],
+      default: "new arrival",
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
